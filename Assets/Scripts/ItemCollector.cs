90077838941c1e4a8f4f89ab1b7dcc70ad5dec39
Julien Lemaire <julien.lemaire@sticmac.fr>
2020-04-18 11:03:11 +0200

    Collectible items

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class ItemCollector : MonoBehaviour
+{
+    [Tag]
+    [SerializeField] string _itemTag;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag(_itemTag)) {
+            Destroy(other.gameObject);
+        }
+    }
+}
