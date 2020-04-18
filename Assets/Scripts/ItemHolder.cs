02a56df334233c409a3e025f957fddaf031ed623
Julien Lemaire <julien.lemaire@sticmac.fr>
2020-04-18 15:00:56 +0200

    Item holder limit

--- Assets/Scripts/ItemHolder.cs
+++ Assets/Scripts/ItemHolder.cs
@@ -1,19 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 public class ItemHolder : MonoBehaviour
 {
     [SerializeField] Transform _itemHolderTransform;
     [Range(0.1f,1f)]
     [SerializeField] float _marginBetweenHeldItems;
+
+    [Header("Item Limit")]
+    [SerializeField] bool _itemLimitEnabled = true;
+    [SerializeField, ShowIf("_itemLimitEnabled")] int _itemLimit = 3;
+
     private Stack<Item> _itemList = new Stack<Item>();
 
     public void AddItem(Item item) {
-        Transform itemTf = item.gameObject.transform;
-        itemTf.SetParent(_itemHolderTransform); 
-        itemTf.localPosition = Vector3.zero + _marginBetweenHeldItems * _itemList.Count * Vector3.up;
-        _itemList.Push(item);
+        if (CanAddItem()) {
+            Transform itemTf = item.gameObject.transform;
+            itemTf.SetParent(_itemHolderTransform); 
+            itemTf.localPosition = Vector3.zero + _marginBetweenHeldItems * _itemList.Count * Vector3.up;
+            _itemList.Push(item);
+        }
+    }
+
+    private bool CanAddItem() {
+        if (!_itemLimitEnabled) {
+            return true;
+        } else {
+            return _itemList.Count < _itemLimit;
+        }
     }
 
     public void Flush() {
