4fd79ec953aafced59509b4a55a155f77a9aedac
Julien Lemaire <julien.lemaire@sticmac.fr>
2020-04-18 13:17:52 +0200

    Basic Level Finished screen

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using NaughtyAttributes;
 
 public class ItemCollector : MonoBehaviour
 {
+
+    public UnityEvent OnAllItemsCollected;
+
     [Tag]
     [SerializeField] string _itemTag;
+
+    [SerializeField] int _maxToCollect = 1;
+
+    private int _collected = 0;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag(_itemTag)) {
             Destroy(other.gameObject);
+            _collected++;
+            if (_collected == _maxToCollect) {
+                OnAllItemsCollected?.Invoke();
+            }
         }
     }
 }
