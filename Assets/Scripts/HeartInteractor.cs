5ee01a40344c128a9606f255261d26f3d42f3927
Julien Lemaire <julien.lemaire@sticmac.fr>
2020-04-18 19:02:04 +0200

    No more action for flushing the items stack

--- Assets/Scripts/HeartInteractor.cs
+++ Assets/Scripts/HeartInteractor.cs
@@ -11,19 +11,10 @@ public class HeartInteractor : MonoBehaviour
 
     [Header("Parameters")]
     [SerializeField, Tag] string _heartTag;
-    private bool _isNearHeart = false;
-
-    public void Give(InputAction.CallbackContext context) {
-        if (context.started && _isNearHeart) {
-            _itemHolder.Flush();
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        _isNearHeart = other.CompareTag(_heartTag);
-    }
-
-    private void OnTriggerExit2D(Collider2D other) {
-        _isNearHeart = false;
+        if (other.CompareTag(_heartTag)) {
+            _itemHolder.Flush();
+        }
     }
 }
