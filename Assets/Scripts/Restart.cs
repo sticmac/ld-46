39b308046e7d3106c98959be1d9c39572f325000
Julien Lemaire <julien.lemaire@sticmac.fr>
2020-04-18 17:40:56 +0200

    Restart button

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public void ApplyRestart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
