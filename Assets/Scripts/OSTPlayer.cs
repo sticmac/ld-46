2d4f19dc05b7a06539a3bdc44bc40b591823b63b
Julien Lemaire <julien.lemaire@sticmac.fr>
2020-04-19 17:52:23 +0200

    SFX and music

---
+++ Assets/Scripts/OSTPlayer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSTPlayer : MonoBehaviour
+{
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+}
